685c7078e44a1e10e98c4e5e4b284172e5b2f8d5
John De Appolonia <jde-appolonia@myseneca.ca>
2023-06-19 13:25:08 -0400

    updated unit tests for multiply

--- tests/multiply.test.js
+++ tests/multiply.test.js
@@ -1,1 +1,16 @@
-// Functionality to be added soon
+const multiply = require('../modules/multiply.js');
+
+test('multiply two positive numbers correctly', () => {
+    const result = multiply(5, 10);
+    expect(result).toBe(50);
+  });
+
+test('multiply a number with zero correctly', () => {
+    const result = multiply(0, 5);
+    expect(result).toBe(0);
+  });
+
+test('multiply a negative number correctly', () => {
+    const result = multiply(4, -1);
+    expect(result).toBe(-4);
+  });
